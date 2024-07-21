6073e4bd3187cb49905d2d0a27445fbf67d9d532
mahmutessiz <mahmutessiz71@gmail.com>
2024-07-21 18:25:56 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const count = ref(0);
+</script>
+
+<template>
+  <div style="
+     display: flex;
+     flex-direction: row;
+     flex-wrap: nowrap;
+     justify-content: center;
+     align-items: center;
+     background-color: black;">
+
+    <button
+     style="
+     color: aliceblue;
+     background-color: black;
+     font-size: 72px;
+     font-weight: 900;
+     border: 0;
+     cursor: zoom-in;
+     width: 100%;
+     "
+     @click="count++">{{ count }}</button>
+     <div style="font-size: 48px; font-weight: 900;
+     cursor: zoom-out;" @click="count--">☠️</div>
+  </div>
+</template>
